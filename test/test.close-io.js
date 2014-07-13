14cb9aae29893902d7a25ec3e35704ac5562c38f
Rick Cerf <rickcerf@gmail.com>
2014-07-12 20:44:05 -0700

    wrote first test

---
+++ test/test.close-io.js
@@ -0,0 +1,23 @@
+var auth         = require('./auth')
+  , facade       = require('segmentio-facade')
+  , helpers      = require('./helpers')
+  , integrations = require('..')
+  , should       = require('should');
+
+
+var close = new integrations['Close.io']()
+  , settings = auth['Close.io'];
+
+
+describe('Close IO', function () {
+
+  describe('.enabled()', function () {
+    var Track = facade.Track;
+    it('should only be enabled for server side messages', function () {
+      keen.enabled(new Track({ channel : 'server' })).should.be.ok;
+      keen.enabled(new Track({ channel : 'client' })).should.not.be.ok;
+      keen.enabled(new Track({})).should.not.be.ok;
+    });
+  });
+});
+

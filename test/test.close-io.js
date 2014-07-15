628578f3c22bd46ec738d915e52e4741e664601c
Rick Cerf <rickcerf@gmail.com>
2014-07-14 22:04:07 -0700

    enabled and verified

--- test/test.close-io.js
+++ test/test.close-io.js
@@ -4,19 +4,41 @@ var auth         = require('./auth')
   , integrations = require('..')
   , should       = require('should');
 
-
-var close = new integrations['Close.io']()
+console.log("Integrations: ", integrations['Close.io']);
+var close    = new integrations['Close.io']()
   , settings = auth['Close.io'];
 
 
-describe('Close IO', function () {
+describe('Close.io', function () {
+  describe('.validate()', function () {
+    it('should require an apiKey', function () {
+      close.validate({}, { apiKey : '' }).should.be.an.instanceOf(Error);
+      close.validate({}, {}).should.be.an.instanceOf(Error);
+      should.not.exist(close.validate({}, { apiKey : 'xxx' }));
+    });
+  });
 
-  describe('.enabled()', function () {
-    var Track = facade.Track;
-    it('should only be enabled for server side messages', function () {
-      close.enabled(new Track({ channel : 'server' })).should.be.ok;
-      close.enabled(new Track({ channel : 'client' })).should.not.be.ok;
-      close.enabled(new Track({})).should.not.be.ok;
+  describe('.validate()', function () {
+    it('should require an apiKey', function () {
+      close.validate({}, { apiKey: ''}).should.be.an.instanceOf(Error);
+      close.validate({}, {}).should.be.an.instanceOf(Error);
+      should.not.exist(close.validate({}, { apiKey: 'xxx'}));
+    });
+  });
+
+  describe('track()', function () {
+    it('should get a good response from the API', function (done) {
+      var track = helpers.track();
+      close.track(track, settings, done);
+    });
+
+    it('will error on an invalid set of keys', function (done) {
+      var track = helpers.track();
+      close.track(track, { apiKey: 'x'}, function (err) {
+        should.exist(err);
+        err.status.should.eql(401);
+        done();
+      });
     });
   });
 });

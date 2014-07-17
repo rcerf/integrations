ae0d5940b3e3c221f32324e5e6b0a3d18d5c099c
Rick Cerf <rickcerf@gmail.com>
2014-07-17 12:34:19 -0700

    working on track, not erroring

--- test/test.close-io.js
+++ test/test.close-io.js
@@ -4,12 +4,54 @@ var auth         = require('./auth')
   , integrations = require('..')
   , should       = require('should');
 
-console.log("Integrations: ", integrations['Close.io']);
 var close    = new integrations['Close.io']()
   , settings = auth['Close.io'];
 
 
 describe('Close.io', function () {
+
+  describe('.enabled()', function () {
+    var Track = facade.Track;
+    it('should require a userId', function () {
+      close.enabled(new Track({
+        channel : 'server'
+      })).should.not.be.ok;
+
+      close.enabled(new Track({
+        userId: 'test@segment.io',
+        channel: 'server'
+      })).should.be.ok;
+    });
+
+    it('should only be enabled for all messages with email', function () {
+      close.enabled(new Track({
+        userId: 'test@segment.io',
+        channel: 'server'
+      })).should.be.ok;
+
+      close.enabled(new Track({
+        userId: 'sss',
+        channel: 'server'
+      })).should.not.be.ok;
+    });
+
+    it('should only be enabled for server side messages', function () {
+      close.enabled(new Track({
+        userId: 'test@segment.io',
+        channel: 'server'
+      })).should.be.ok;
+
+      close.enabled(new Track({
+        userId: 'test@segment.io',
+        channel: 'client'
+      })).should.not.be.ok;
+
+      close.enabled(new Track({
+        userId: 'test@segment.io'
+      })).should.not.be.ok;
+    });
+  });
+
   describe('.validate()', function () {
     it('should require an apiKey', function () {
       close.validate({}, { apiKey : '' }).should.be.an.instanceOf(Error);
@@ -18,11 +60,10 @@ describe('Close.io', function () {
     });
   });
 
-  describe('.validate()', function () {
-    it('should require an apiKey', function () {
-      close.validate({}, { apiKey: ''}).should.be.an.instanceOf(Error);
-      close.validate({}, {}).should.be.an.instanceOf(Error);
-      should.not.exist(close.validate({}, { apiKey: 'xxx'}));
+  describe('.identify()', function () {
+    it('should get a good response from the API', function (done) {
+      var identify = helpers.identify();
+      close.identify(identify, settings, done);
     });
   });
 
@@ -35,6 +76,7 @@ describe('Close.io', function () {
     it('will error on an invalid set of keys', function (done) {
       var track = helpers.track();
       close.track(track, { apiKey: 'x'}, function (err) {
+        console.log("ERROR: ", err);
         should.exist(err);
         err.status.should.eql(401);
         done();
